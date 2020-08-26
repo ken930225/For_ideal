a56cd9cf041105e282e85d58781d36170b752de3
vagrant <vagrant@localhost.localdomain>
2020-08-26 14:41:49 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_014528) do
+ActiveRecord::Schema.define(version: 2020_08_26_123837) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+    t.index ["workout_id"], name: "index_favorites_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "introduction"
+    t.string "objective"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "workout_comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_workout_comments_on_user_id"
+    t.index ["workout_id"], name: "index_workout_comments_on_workout_id"
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.integer "user_id"
@@ -21,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_014528) do
     t.integer "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "date"
   end
 
 end
