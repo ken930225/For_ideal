19cc48fb1bcad4429eb1697eab218d40dcef60d7
EC2 Default User <ec2-user@ip-172-31-39-84.ap-northeast-1.compute.internal>
2020-08-29 12:12:23 +0000

    update migrations

--- db/migrate/20200825062704_create_favorites.rb
+++ db/migrate/20200825062704_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
+      t.references :user, type: :bigint, foreign_key: true
+      t.references :workout, type: :bigint, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200825071756_rename_post_id_column_to_favorites.rb
+++
@@ -1,5 +0,0 @@
-class RenamePostIdColumnToFavorites < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :favorites, :post_id, :workout_id
-  end
-end
